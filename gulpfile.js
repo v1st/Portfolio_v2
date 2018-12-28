2613e0cb2dc2faae63dabb1f80828e747a8aeee0
v1st <brandon_m918@yahoo.com>
2018-12-28 11:25:15 -0500

    updated tilt js for projects

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,11 @@
-'use strict';
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-gulp.task('sass', function () {
-  return gulp.src('src/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('src/css'));
-});
- 
-gulp.task('watch', function () {
+gulp.task('sass', () => gulp.src('src/scss/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('src/css')));
+
+gulp.task('watch', () => {
   gulp.watch('src/scss/**/*.scss', ['sass']);
 });
 
