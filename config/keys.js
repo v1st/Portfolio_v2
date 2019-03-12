00214607c6fe288da94a6f227dec7103bde23726
v1st <brandon_m918@yahoo.com>
2019-03-12 09:02:20 -0400

    heroku config update

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,8 @@
-const prod = require('./prod');
-const dev = require('./dev');
-
 // keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
   // we are in production - return the prod set of keys
-  module.exports = prod;
+  module.exports = require('./prod');
 } else {
   // we are in development - return the dev keys!!
-  module.exports = dev;
+  module.exports = require('./dev');
 }
